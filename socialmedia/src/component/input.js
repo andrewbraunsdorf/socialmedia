378685f26eead346bcc8fbc7af67d24aaa586d82
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-04-10 20:59:02 +0000

    set state and added display/input.js files

---
+++ socialmedia/src/component/input.js
@@ -0,0 +1,19 @@
+    
+import React, {Component} from "react";
+import axios from "axios";
+
+class Input extends Component{
+  state={
+    artistname: null,
+    songname: null,
+    lyrics: null
+  }
+  
+render(){
+      return(
+        <div>
+        
+        </div>
+      )
+    }
+}
